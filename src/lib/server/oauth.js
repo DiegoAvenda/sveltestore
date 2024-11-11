b67115475964c602c7744d8a3576123d968710a1
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2024-11-10 19:11:53 -0600

    production

--- src/lib/server/oauth.js
+++ src/lib/server/oauth.js
@@ -4,5 +4,5 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 export const google = new Google(
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
-	'http://localhost:5173/api/oauth/google/callback'
+	'https://sveltestore-gamma.vercel.app/api/oauth/google/callback'
 );
